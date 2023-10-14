0c1286a11033766a48e1c367cda654d82c98501f
Gurmeet Athwal <gurmeet.athwal@gmail.com>
2023-10-14 11:35:00 +0530

    update dealership and review functions

--- functions/dealership.js
+++ functions/dealership.js
@@ -1,64 +1,59 @@
-// Importing the cloudant package
-const Cloudant = require('@ibm-cloud/cloudant');
+/**
+  *
+  * main() will be run when you invoke this action
+  *
+  * @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+  *
+  * @return The output of this action, which must be a JSON object.
+  *
+  */
+const { CloudantV1 } = require('@ibm-cloud/cloudant');
+const { IamAuthenticator } = require('ibm-cloud-sdk-core');
 
-// Initializing the Cloudant client with provided credentials
-const cloudant = Cloudant({
-  url: process.env.COUCH_URL,
-  plugins: { iamauth: { iamApiKey: process.env.IAM_API_KEY } }
-});
-
-// Accessing the 'dealerships' database
-const db = cloudant.db.use('dealerships');
-
-// IBM Cloud Function handler function
 async function main(params) {
-  try {
-    // Extracting query parameters from the input 'params' object
-    const stateFilter = params.state || null;
-    const dealerIdFilter = params.dealerId || null;
 
-    // Fetching a list of documents from the database
-    const data = await db.list({ include_docs: true });
+    const authenticator = new IamAuthenticator({apikey: params.IAM_API_KEY});
+    const service = new CloudantV1({ authenticator });
+    service.setServiceUrl(params.COUCH_URL);
 
-    // Handling the case where the database is empty
-    if (data.rows.length === 0) {
-      return {
-        statusCode: 404,
-        body: { message: 'The database is empty' }
-      };
-    }
+    const state = params.state;
 
-    // Filtering and mapping the data based on the query parameters
-    const dealerships = data.rows
-      .filter(row => {
-        if (stateFilter && dealerIdFilter) {
-          return row.doc.state === stateFilter && row.doc.id === parseInt(dealerIdFilter);
-        } else if (stateFilter) {
-          return row.doc.state === stateFilter;
-        } else if (dealerIdFilter) {
-          return row.doc.id === parseInt(dealerIdFilter);
-        } else {
-          return true;
+    try {
+        const inputDocuments = await service.postAllDocs({
+            db: 'dealerships',
+            includeDocs: true
+        });
+
+        const rows = inputDocuments.result.rows;
+
+        if (rows.length === 0){
+            return {statusCode: 404, body: "The database is empty"};
         }
-      })
-      .map(row => {
-        const { id, city, state, st, address, zip, lat, long, short_name, full_name } = row.doc;
-        return { id, city, state, st, address, zip, lat, long, short_name, full_name };
-      });
 
-    // Returning a successful response with the filtered and mapped data
-    return {
-      statusCode: 200,
-      body: dealerships
-    };
-  } catch (error) {
-    // Handling errors and returning a 500 status code with an error message
-    return {
-      statusCode: 500,
-      body: { message: 'Something went wrong on the server', error: error.message }
-    };
-  }
+        const outputArray = rows.map(({ doc }) => {
+            if (state === undefined || doc.state === state){
+                return {
+                    id: doc.id,
+                    city: doc.city,
+                    state: doc.state,
+                    st: doc.st,
+                    address: doc.address,
+                    zip: doc.zip,
+                    lat: doc.lat,
+                    long: doc.long
+                };
+            }
+            return null; // Filter out unwanted items
+        }).filter(item => item !== null);
+
+        if(outputArray.length === 0){
+            return {statusCode: 404, body: "The state does not exist"};
+        }
+
+        return {statusCode: 200, body: outputArray };
+    } catch (error) {
+        return {statusCode: 500, body: "Something went wrong on the server"};
+    }
 }
 
-// Exporting the main function for use in IBM Cloud Function
 module.exports = { main };
--- functions/review.py
+++ functions/review.py
@@ -1,58 +1,49 @@
-import os
+#
+#
+# main() will be run when you invoke this action
+#
+# @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+#
+# @return The output of this action, which must be a JSON object.
+#
+#
 import json
-from cloudant.client import Cloudant
+from ibmcloudant.cloudant_v1 import CloudantV1, Document
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-api_user = os.environ['COUCH_USERNAME']
-api_password = os.environ['IAM_API_PASSWORD']
-api_url = os.environ['COUCH_URL']
+authenticator = None
+service = None
 
-def list_reviews(dealer_id=None):
-    # Initialize Cloudant connection using the provided credentials
-    client = Cloudant(api_user, api_password, url=api_url)
-    client.connect()
+# Initialize Cloudant connection using the provided credentials
+def initialize_authenticator_and_service(apikey, url):
+    global authenticator, service
+    authenticator = IAMAuthenticator(apikey)
+    service = CloudantV1(authenticator=authenticator)
+    service.set_service_url(url)
 
-    # Access the 'reviews' database
-    database = client['reviews']
-
-    # Initialize the result list
-    result = []
-
-    # If dealer_id is provided, filter by 'dealership'
-    if dealer_id is not None:
-        for document in database:
-            if document.get('dealership') == int(dealer_id):
-                result.append(document)
-    else:
-        # If no dealer_id is provided, list all items
-        for document in database:
-            result.append(document)
-
-    # Disconnect from Cloudant
-    client.disconnect()
-
-    return {
-        'statusCode': 200,
-        'body': json.dumps(result)
-    }
-
-def add_review(data):
+def list_reviews(dealer_id):
+    global authenticator, service
     try:
-        # Initialize Cloudant connection using the provided credentials
-        client = Cloudant(api_user, api_password, url=api_url)
-        client.connect()
-
         # Access the 'reviews' database
-        database = client['reviews']
+        db_name = 'reviews'
+        database = service.post_all_docs(db=db_name,include_docs=True).get_result()
 
-        # Insert the new document
-        doc = database.create_document(data['review'])
+        # Initialize the result list
+        result = []
 
-        # Disconnect from Cloudant
-        client.disconnect()
+        # If dealer_id is provided, filter by dealer_id
+        if dealer_id is not None:
+            for document in database['rows']:
+                if document.get('doc', {}).get('dealership') == int(dealer_id):  # Check if 'doc' exists
+                    result.append(document.get('doc'))
+        else:
+            # If no dealer_id is provided, list all items
+            for document in database['rows']:
+                result.append(document.get('doc'))
 
         return {
             'statusCode': 200,
-            'body': json.dumps({'message': 'Document added successfully', '_id': doc['_id']})
+            'body': json.dumps(result)
         }
     except Exception as e:
         return {
@@ -60,29 +51,59 @@ def add_review(data):
             'body': json.dumps(f'Something went wrong on the server: {str(e)}')
         }
 
-def main(arg):
-    try:
-        if arg.get('httpMethod') == "GET":
-            if arg.get('queryStringParameters') == None:
-                return list_reviews()
-            else:
-                query_parameters = arg.get('queryStringParameters')
-                if query_parameters.get('dealerId') == None:
-                    return list_reviews()
-                else:
-                    dealer_id = query_parameters.get('dealerId')
-                    result = list_reviews(dealer_id)
 
-                    if result['statusCode'] == 200 and len(json.loads(result['body'])) == 0:
-                        return {
-                            'statusCode': 404,
-                            'body': json.dumps('dealerId does not exist')
-                        }
-                    else:
-                        return result
-        elif arg.get('httpMethod') == "POST":
-            request_body = json.loads(arg.get('body'))
-            result = add_review(request_body)
+def add_review(doc_data):
+    global authenticator, service
+    try:
+        # Access the 'reviews' database
+        db_name = 'reviews'
+        db_information = service.get_database_information(db=db_name).get_result()
+
+        # get total document count
+        doc_count = db_information["doc_count"]
+
+        # Insert the 'id' attribute
+        doc_data["id"] = doc_count + 1
+        # Insert the new document
+        response = service.post_document(db=db_name, document=doc_data).get_result()
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'message': 'Document added successfully', '_id': response['id']})
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps(f'Something went wrong on the server: {str(e)}')
+        }
+
+def main(param_dict):
+    try:
+        api_key = param_dict['IAM_API_KEY']
+        api_url = param_dict['COUCH_URL']
+
+        initialize_authenticator_and_service(api_key, api_url)
+
+        method = param_dict['__ow_method']
+
+        if method == 'get':
+            if 'dealerId' not in param_dict:
+                return list_reviews(None)
+            else:
+                result = list_reviews(param_dict['dealerId'])
+
+                # Check if dealer_id exists
+                if result['statusCode'] == 200 and len(json.loads(result['body'])) == 0:
+                    return {
+                        'statusCode': 404,
+                        'body': json.dumps('dealerId does not exist')
+                    }
+                else:
+                    return result
+        elif  method == "post":
+            # Extract data from the request body
+            request_doc = param_dict["review"]
+            result = add_review(request_doc)
             return result
         else:
             return {
@@ -94,3 +115,4 @@ def main(arg):
             'statusCode': 500,
             'body': json.dumps(f'Something went wrong on the server: {str(e)}')
         }
+
