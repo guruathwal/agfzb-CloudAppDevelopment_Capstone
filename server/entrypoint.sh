a1c53919ab3cd9b4722d0800f65815028c2e57ba
Gurmeet Athwal <gurmeet.athwal@gmail.com>
2023-10-18 19:24:53 +0530

    Update entrypoint.sh

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,19 +1,19 @@
-    #!/bin/sh
+#!/bin/sh
 
-    if [ "$DATABASE" = "postgres" ]; then
-        echo "Waiting for postgres..."
+if [ "$DATABASE" = "postgres" ]; then
+    echo "Waiting for postgres..."
 
-        while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
-        sleep 0.1
-        done
+    while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
+    sleep 0.1
+    done
 
-        echo "PostgreSQL started"
-    fi
+    echo "PostgreSQL started"
+fi
 
-    # Make migrations and migrate the database.
-    echo "Making migrations and migrating the database. "
-    python manage.py makemigrations main --noinput
-    python manage.py migrate --noinput
-    python manage.py makemigrations djangoapp --noinput
-    python manage.py migrate djangoapp --noinput
-    exec "$@"
+# Make migrations and migrate the database.
+echo "Making migrations and migrating the database. "
+python manage.py makemigrations main --noinput
+python manage.py migrate --noinput
+python manage.py makemigrations djangoapp --noinput
+python manage.py migrate djangoapp --noinput
+exec "$@"
